14db45f947d6ff4b8562e22fecf548d2557b23e0
Dev43 <patrick.guay43@gmail.com>
2017-02-20 11:57:38 -0500

    add product specs for validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -66,16 +66,3 @@ RSpec.describe Product, type: :model do
 
   end
 end
-
-
-
-#  create_table "products", force: :cascade do |t|
-  #   t.string   "name"
-  #   t.text     "description"
-  #   t.string   "image"
-  #   t.integer  "price_cents"
-  #   t.integer  "quantity"
-  #   t.datetime "created_at",  null: false
-  #   t.datetime "updated_at",  null: false
-  #   t.integer  "category_id"
-  # end
