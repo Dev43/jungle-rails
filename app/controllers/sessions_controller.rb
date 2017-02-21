5d4bf3798a01fd2173f39994e7cb50c37748c66e
Dev43 <patrick.guay43@gmail.com>
2017-02-21 10:47:58 -0500

    new user in sessions new method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
